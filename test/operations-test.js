b6b695bddc45202fabcc6f93b174cf5ef4ea992b
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-10-23 11:51:27 +0200

    add more unit tests

--- test/operations-test.js
+++ test/operations-test.js
@@ -6,9 +6,20 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 var mockPackage = { foo: 'bar' };
+var mockContext = { foo: 'bar' };
 var operations = require.main.require('lib/operations')(mockPackage);
 
 describe('hello-service', () => {
+    describe('sendPackage', function() {
+        it('should return package.json and a set of services', function() {
+            expect(capture(operations.sendPackage, {}))
+                .to.deep.include({
+                    'package.json': mockPackage,
+                    operations: [ 'hello', 'echo' ]
+                });
+        });
+    });
+
     describe('sayHello', function() {
         it('should say hello to anonymous caller', function() {
             expect(capture(operations.sayHello, {}))
@@ -24,6 +35,23 @@ describe('hello-service', () => {
             expect(capture(operations.sayHello, { body: { name: 'foo' } }))
                 .to.include({ greeting: 'Hello, Foo!' })
                 .and.not.to.have.property('hint');
+        });
+    });
+
+    describe('echoContext', function() {
+        it('should echo the context from the request header', function(done) {
+            expect(pcapture(operations.echoContext, {
+                header: function() { return JSON.stringify(mockContext); }
+            })).to.eventually.deep.become(mockContext).notify(done);
+        });
+        it('should return an error when the context is missing', function(done) {
+            expect(pcapture(operations.echoContext, { header: function() {} }))
+                .to.eventually.have.property('error').notify(done);
+        });
+        it('should return an error when the context is not json', function(done) {
+            expect(pcapture(operations.echoContext, {
+                header: function() { return 'My name is Jason, not json.'; }
+            })).to.eventually.have.property('error').notify(done);
         });
     });
 });
