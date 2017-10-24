babf79b41af5b28c5fcb5c6167054298b41cc393
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-10-24 11:36:21 +0200

    fix index.js implementation

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ var bunyan = require('bunyan');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var package = require(process.env[ 'PWD' ] + '/package.json');
-var config = package.config;
+var packageJson = require(process.env[ 'PWD' ] + '/package.json');
+var config = packageJson.config;
 var logger = bunyan.createLogger({
     name: config.serviceName,
     level: config.logLevel
 });
 
-var methods = require('./lib/methods.js')(package);
+var operations = require('./lib/operations.js')(packageJson);
 
 var app = express();
 app.listen(8080, function() {
     logger.info('service ready');
 });
 
-app.get('/', methods.sendPackage);
-app.get('/hello', methods.sayHello);
-app.post('/hello', bodyParser.json(), methods.sayHello);
-app.get('/echo', methods.echoContext);
-app.post('/echo', methods.echoContext);
+app.get('/', operations.sendPackage);
+app.get('/hello', operations.sayHello);
+app.post('/hello', bodyParser.json(), operations.sayHello);
+app.get('/echo', operations.echoContext);
+app.post('/echo', operations.echoContext);
