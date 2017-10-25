20abf24cc7253211d623b1bda2e0c60a43e2e4f9
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-10-25 09:19:59 +0200

    refactor unit tests

--- test/operations-test.js
+++ test/operations-test.js
@@ -12,7 +12,7 @@ var operations = require.main.require('lib/operations')(mockPackage);
 describe('hello service', () => {
     describe('sendPackage', function() {
         it('should return package.json and a set of services', function() {
-            expect(capture(operations.sendPackage, {}))
+            expect(capture(operations.sendPackage, {}).body)
                 .to.deep.include({
                     'package.json': mockPackage,
                     operations: [ 'greet', 'echo' ]
@@ -22,17 +22,17 @@ describe('hello service', () => {
 
     describe('greet', function() {
         it('should say hello to anonymous caller', function() {
-            expect(capture(operations.greet, {}))
+            expect(capture(operations.greet, {}).body)
                 .to.include({ greeting: 'Hello, World!' })
                 .and.to.have.property('hint').that.matches(/^Try including a name/);
         });
         it('should say hello to name via query parameter', () => {
-            expect(capture(operations.greet, { query: { name: 'foo' } }))
+            expect(capture(operations.greet, { query: { name: 'foo' } }).body)
                 .to.include({ greeting: 'Hello, Foo!' })
                 .and.not.to.have.property('hint');
         });
         it('should say hello to name via post body', () => {
-            expect(capture(operations.greet, { body: { name: 'foo' } }))
+            expect(capture(operations.greet, { body: { name: 'foo' } }).body)
                 .to.include({ greeting: 'Hello, Foo!' })
                 .and.not.to.have.property('hint');
         });
@@ -42,28 +42,28 @@ describe('hello service', () => {
         it('should echo the context from the request header', function(done) {
             expect(pcapture(operations.echoContext, {
                 header: function() { return JSON.stringify(mockContext); }
-            })).to.eventually.deep.become(mockContext).notify(done);
+            })).to.eventually.deep.become({ body: mockContext }).notify(done);
         });
         it('should return an error when the context is missing', function(done) {
             expect(pcapture(operations.echoContext, { header: function() {} }))
-                .to.eventually.have.property('error').notify(done);
+                .to.eventually.have.nested.property('body.error').notify(done);
         });
         it('should return an error when the context is not json', function(done) {
             expect(pcapture(operations.echoContext, {
                 header: function() { return 'My name is Jason, not json.'; }
-            })).to.eventually.have.property('error').notify(done);
+            })).to.eventually.have.nested.property('body.error').notify(done);
         });
     });
 });
 
 function capture(op, request) {
-    var result;
-    op(request, { send: function(r) { result = r; } });
-    return result;
+    var response = {};
+    op(request, { send: function(r) { response.body = r; } });
+    return response;
 }
 
 function pcapture(op, request) {
-    var result;
-    return op(request, { send: function(r) { result = r; } })
-        .then(function() { return result; });
+    var response = {};
+    return op(request, { send: function(r) { response.body = r; } })
+        .then(function() { return response; });
 }
